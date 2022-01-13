af98abb9a04dff394acc7bbe5a97b8e7646ae4d2
Ron Kosova <ron.kosova@gmail.com>
2022-01-13 02:23:21 +0200

    Added strdstrtod function

---
+++ h/strdstrtod.h
@@ -0,0 +1,2 @@
+
+double* strdstrtod(char **in, int column_number,int class_column);
---
+++ src/strdstrtod.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+double* strdstrtod(char **in, int column_number,int class_column)
+{
+	double *out = malloc(sizeof(double) * column_number);
+
+	if(class_column>column_number) {
+		return NULL;
+	} else {
+		for(int i=0; i<column_number; i++) {
+			if(i==class_column) continue;
+			*(out+i) = strtod(*(in+i), NULL);
+		}
+
+		return out;
+	}
+
+	return NULL;
+}
+
+
