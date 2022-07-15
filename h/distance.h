b596b76b7df72003fc13147e8dbc211c4154a362
Ron Kosova <ron.kosova@gmail.com>
2022-07-15 04:45:21 +0300

    Added include guards

--- h/distance.h
+++ h/distance.h
@@ -1,2 +1,6 @@
+#ifndef DISTANCE_H
+#define DISTANCE_H
 
 double distance(double *unclassified_vector, double *cassified_vector, int dimension);
+
+#endif
--- h/strdstrtod.h
+++ h/strdstrtod.h
@@ -1,2 +1,6 @@
+#ifndef STRDSTRTOD_H
+#define STRDSTRTOD_H
 
 double* strdstrtod(char **in, int column_number,int class_column);
+
+#endif
